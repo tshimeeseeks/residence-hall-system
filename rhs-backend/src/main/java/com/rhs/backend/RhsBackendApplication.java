8e522605e593761a455d881f7eea2269524969c7
Meeseeks <sir.meeseeks@proton.me>
2025-08-21 13:09:48 +0200

    Initial commit

---
+++ rhs-backend/src/main/java/com/rhs/backend/RhsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rhs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhsBackendApplication.class, args);
+	}
+
+}
---
+++ rhs-backend/src/test/java/com/rhs/backend/RhsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
